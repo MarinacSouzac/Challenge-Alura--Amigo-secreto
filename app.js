7c7e447881ba48787773dcf737688178378b8ac1
Marina Souza <marina.csouza01@gmail.com>
2025-03-16 21:38:38 -0300

    Implementação de funções de inserir nome e validar entrada

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//declaração de variável lista de nome de amigos
+let amigos=[];
+
+//Implemnetar função para  inserir nome de amigo
+function inserirNome()
+{
+    let nomeInserido=document.querySelector('input').value;
+    validarEntrada(nomeInserido);
+    
+
+    
+
+
+}
+//Implemnetar função para validar Entrada
+function validarEntrada(nome)
+{
+    if(nome=='')
+    {
+     alert("Por favor,insira um nome.");
+     return false;
+    }
+    return true;
+}
